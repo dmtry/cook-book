59824e497aa69d7b63c9a5c25c7a7be5d68a0b70
Ubuntu <ubuntu@ip-172-31-44-177.ec2.internal>
2019-07-17 13:40:20 +0000

    added new pages and relevant folders: single and multiple recipes; added style.css, added js and materialize libraries; added relevant routed to app.py

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+# db connect
+app.config["MONGO_DBNAME"] = 'cook_book'
+app.config["MONGO_URI"] = 'mongodb+srv://root:DOOMiddqd666@myfirstcluster-labih.mongodb.net/cook_book?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+#get recipes page
 @app.route('/')
-def cookbook():
-    return 'CookBook!'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    
+#get single recipe page
+@app.route('/')
+@app.route('/get_recipe')
+def get_recipe():
+    return render_template("recipe.html", recipes=mongo.db.recipes.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
