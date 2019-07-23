32b85fdb78d02baa4849ed9b40d244b5af34feb9
Ubuntu <ubuntu@ip-172-31-44-177.ec2.internal>
2019-07-23 09:29:10 +0000

    added: 1. add_recipe and edit_recipe html files and  relevant routes 2. added temp folder to assets for temp code experiments 3. updated base,recipe,recipes.html to reflect recipe edit option, card layput on main page, redirect to single recipe view 4. added new routes to app.py - view, edit, add and save new recipes

--- app.py
+++ app.py
@@ -18,10 +18,31 @@ def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
     
 #get single recipe page
-@app.route('/')
-@app.route('/get_recipe')
-def get_recipe():
-    return render_template("recipe.html", recipes=mongo.db.recipes.find())
+@app.route('/get_recipe/<recipe_id>')
+def get_recipe(recipe_id):
+    return render_template("recipe.html", recipes=mongo.db.recipes.find({"_id": ObjectId(recipe_id)}))
+
+    
+#add new recipe page
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add_recipe.html', 
+        categories=mongo.db.categories.find())
+
+#submit new recipe
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+
+#edit recipe
+@app.route ('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    #the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template("edit_recipe.html", recipe=the_recipe, categories=all_categories)
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
